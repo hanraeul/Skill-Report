746ef3da767d551b31b0d3eaee66cda5a0327679
hanraeul <toast17@naver.com>
2024-07-03 21:40:30 +0900

    init commit

---
+++ src/main/java/com/sparta/skillproject/SkillProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.skillproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillProjectApplication.class, args);
+	}
+
+}
